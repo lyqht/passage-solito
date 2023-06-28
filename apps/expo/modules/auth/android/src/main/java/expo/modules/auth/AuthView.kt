c494904afe62cead31098bc5cb3d5b98052033ba
Estee <estee.tsw@gmail.com>
2023-06-28 19:52:34 +0800

    remove boilerplate views

--- apps/expo/modules/auth/android/src/main/java/expo/modules/auth/AuthView.kt
+++
@@ -1,7 +0,0 @@
-package expo.modules.auth
-
-import android.content.Context
-import expo.modules.kotlin.AppContext
-import expo.modules.kotlin.views.ExpoView
-
-class AuthView(context: Context, appContext: AppContext) : ExpoView(context, appContext)
--- apps/expo/modules/auth/index.ts
+++ apps/expo/modules/auth/index.ts
@@ -1,26 +1,7 @@
-import { NativeModulesProxy, EventEmitter, Subscription } from 'expo-modules-core';
-
 // Import the native module. On web, it will be resolved to Auth.web.ts
 // and on native platforms to Auth.ts
 import AuthModule from './src/AuthModule';
-import AuthView from './src/AuthView';
-import { ChangeEventPayload, AuthViewProps } from './src/Auth.types';
-
-// Get the native constant value.
-export const PI = AuthModule.PI;
 
 export function hello(): string {
   return AuthModule.hello();
 }
-
-export async function setValueAsync(value: string) {
-  return await AuthModule.setValueAsync(value);
-}
-
-const emitter = new EventEmitter(AuthModule ?? NativeModulesProxy.Auth);
-
-export function addChangeListener(listener: (event: ChangeEventPayload) => void): Subscription {
-  return emitter.addListener<ChangeEventPayload>('onChange', listener);
-}
-
-export { AuthView, AuthViewProps, ChangeEventPayload };
